21de2c47b765e96be26e8b026559519ea85ee57a
Alexander Naberezhnyi <naberezhnyy.an@gmail.com>
2022-09-08 12:30:25 +0500

    add new files

--- for.js
+++ for.js
@@ -1,25 +1,5 @@
-let newTrans = [{ a: 1000, b: 25.11}, { a: 500, b: 2 }, { a: 135, b: 2 }, { a: 253, b: 2 }, { a: 300, b: 2 }]
-let lastTrans = [{ a: 135, b: 2 }, { a: 253, b: 2 }, { a: 300, b: 2 }, { a: 100, b: 2 }, { a: 110, b: 2 }]
-
-let count = 0
-
-for (i = 0; i < newTrans.length; ++i) {
-    for (let j = 0; j < lastTrans.length; ++j) {
-        if (newTrans[i].a && newTrans[i].a !== lastTrans[j].a && newTrans[j].b) {
-            ++count
-        }
-    }
-    if (count == lastTrans.length) {
-        console.log({
-            value: newTrans[i].a,
-            time: newTrans[i].b
-        })
-    }
-    count = 0
+for (var i = 1; i <= 5; i++) {
+    setTimeout(function timer() {
+        console.log(i);
+    }, i * 1000);
 }
-
-
-
-//Практическая задача , выяснить как сравнить 2 массива
-
-
