ecddb876e7f76f28fd8862bb54f32955f837bae0
Alexander Naberezhnyi <naberezhnyy.an@gmail.com>
2022-09-10 11:21:22 +0500

    experiment with for.js

--- for.js
+++ for.js
@@ -1,17 +1,65 @@
-for (var i = 1; i <= 5; i++) {
-    (function (j) {
-        setTimeout(function timer() {
-            console.log(j);
-        }, 0);
-    })(i);
-    console.log('Hi')
-    console.log('Hi')
-    console.log('Hi')
-    console.log('Hi')
-    console.log('Hi')
-    console.log('Hi')
-    console.log('Hi')
-    console.log('Hi')
-    console.log('Hi')
-    console.log('Hi')
-}
+// for (var i = 1; i <= 5; i++) {
+//     (function (j) {
+//         setTimeout(function timer() {
+//             console.log(j);
+//         }, 0);
+//     })(i);
+//     console.log('Hi')
+//     console.log('Hi')
+//     console.log('Hi')
+//     console.log('Hi')
+//     console.log('Hi')
+//     console.log('Hi')
+//     console.log('Hi')
+//     console.log('Hi')
+//     console.log('Hi')
+//     console.log('Hi')
+// }
+
+// console.log('Hi')
+// console.log('Hi')
+// console.log('Hi')
+// setTimeout(() => {
+//     console.log('Я тайм-аут')
+// })
+// console.log('Bye')
+// console.log('Bye')
+// console.log('Bye')
+// console.log('Bye')
+// function qwe() {
+//     setTimeout(() => {
+//         console.log('Тайм аут в функции qwe')
+//     })
+//     console.log('Я функций qwe')
+// }
+// qwe()
+// setTimeout(() => {
+//     console.log('Я тайм-аут 2')
+// })
+
+// for (let i = 0; i < 5; ++i) {
+//     setTimeout(() => {
+//         console.log(i)
+//     })
+//     console.log('console.log')
+// }
+setTimeout(() => {
+    console.log('Timeout')
+})
+console.log('console.log')
+setTimeout(() => {
+    console.log('Timeout2')
+})
+console.log('console.log')
+setTimeout(() => {
+    console.log('Timeout3')
+})
+console.log('console.log')
+setTimeout(() => {
+    console.log('Timeout4')
+})
+console.log('console.log')
+setTimeout(() => {
+    console.log('Timeout5')
+})
+console.log('console.log')
