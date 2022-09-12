47cbc5f63b6bb710f75e3cd8528228361e9e56ae
Alexander Naberezhnyi <naberezhnyy.an@gmail.com>
2022-09-12 18:48:55 +0500

    update multi

--- excercise/multiples3&5.js
+++ excercise/multiples3&5.js
@@ -1,34 +1,37 @@
 function solution(number) {
     if (number < 0) { return 0 }
 
-    var multi3 = []
-    var multi5 = []
+    var allNumbers = [];
 
     for (let i = 0; i <= number; ++i) {
         let RESULT_3 = i / 3
         let RESULT_5 = i / 5
         if (!String(RESULT_3).includes('.')) {
-            multi3.push(i)
-            console.log(i)
+            allNumbers.push(i)
         }
         if (!String(RESULT_5).includes('.')) {
-            multi5.push(i)
-            console.log(i)
+            allNumbers.push(i)
         }
     }
-    var sumArr = [...multi3, ...multi5]
-    var uniqArr = sumArr.filter(function (item, pos) {
-        return sumArr.indexOf(item) == pos;
+    var uniqArr = allNumbers.filter(function (item, pos) {
+        return allNumbers.indexOf(item) == pos;
     })
-    console.log(uniqArr)
-    if (uniqArr.length != 0) { return result = uniqArr.reduce((acc, cur) => { return acc + cur }) } else { return false }
-
-    // console.log(multi3)
-    // console.log(multi5)
-    // console.log(uniqArr)
-    // console.log(result)
+    // if (uniqArr.length != 0 ) {
+    //     uniqArr.reduce((acc,cur) => {
+    //         var sum = 0;
+    //         var plus = 0;
+    //         if (sum < number) {
+    //             if ()
+    //             plus += acc + cur
+    //         } 
+    //     })
+    // } else {
+    //     return false
+    // }
+    
+    // if (uniqArr.length != 0) { return result = uniqArr.reduce((acc, cur) => { return acc + cur }) } else { return false }
 }
 
 
-let a = solution(0)
+let a = solution(1000)
 console.log(a)
