6edf3530604a330c3562603938b68c8fb3a3211a
Alexander Naberezhnyi <naberezhnyy.an@gmail.com>
2022-09-13 17:36:33 +0500

    try solve excercise with reduce

--- excercise/reduce-test.js
+++ excercise/reduce-test.js
@@ -2,6 +2,8 @@
 Find max equal value to <=10 but not more
 */
 
+
+// Solution with for
 let arr = [5, 4, 3, 2, 1, 6, 8, 9]
 let result = 0;
 
@@ -13,5 +15,19 @@ for (let i = 0; i < arr.length; i++) {
     } else if (result + elOfArr > 10) {
     }
 }
-
 console.log(result)
+
+
+
+// let res2 = 0;
+// arr.reduce((acc,cur) => {
+//     if (acc + cur < 10) {
+//         res2 += acc + cur
+//         return acc + cur
+//     } else if (acc + cur > 10) {
+//         return cur
+//     }
+// },0)  
+
+
+// console.log(res2)
