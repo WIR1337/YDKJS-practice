f717b4577c5352dad246fa36006d7497e37f2fcd
Alexander Naberezhnyi <naberezhnyy.an@gmail.com>
2022-09-19 11:42:13 +0500

    add new folder

---
+++ lexicalEnviroonment/polyfil.js
@@ -0,0 +1,19 @@
+// {
+//     let a = 2
+//     console.log(a)
+// }
+
+// console.log(a) // referenceError
+
+
+// {
+//     var a = 2
+//     console.log(a)
+// }
+
+// console.log(a) // 2
+
+
+
+let a = b => { console.log(`Anonymous ${b}`) }
+a('Alexander')
---
+++ lexicalEnviroonment/this.js
@@ -0,0 +1,13 @@
+var obj = {
+    id: "awesome",
+    cool: function coolFn() {
+        console.log(this.id);
+    }
+};
+
+var id = "not awesome";
+
+obj.cool(); // awesome
+
+setTimeout(obj.cool, 100); // not awesome
+
